516b764bef31daae3342b74b10d00fbd4cf215b5
Naim <Naim@DESKTOP-CBOOKB5>
2023-01-02 19:05:45 +0000

    First version of kata4

--- src/kata4/main/Loader.java
+++
@@ -1,5 +0,0 @@
-package kata4.main;
-
-public interface Loader {
-    Iterable<String> items();
-}
--- src/kata4/model/Mail.java
+++ src/kata4/model/Mail.java
@@ -3,25 +3,24 @@ package kata4.model;
 import java.util.regex.Pattern;
 
 public class Mail {
-    private String mail;
-    //private static final Pattern pattern = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
-    private static final Pattern pattern = Pattern.compile("^[\\w-\\.]+@");
+    private final String mail;
+    private static final Pattern pattern = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+    private static final Pattern userNamepattern = Pattern.compile("^[\\w-\\.]+@");
 
     public Mail(String mail) {
         this.mail = mail;
     }
     
     public String getDomain(){
-        /*if (pattern.matcher(this.mail).matches() == true) return this.mail;
-        else {return null;}*/
-        String[] domain = pattern.split(this.mail);
-        //System.out.println(pattern.split(this.mail));
+        String[] domain = userNamepattern.split(this.mail);
         String result = "";
         for (String car : domain) 
             result = result + car;
         return result;
     }
     
-    
+    public static boolean isEmail(String line) {
+        return pattern.matcher(line).matches();
+    }
     
 }
